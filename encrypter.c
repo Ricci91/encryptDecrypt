45109715b4223ce028d06b6a3604df9ee666f26b
Ricci91 <froyland.richard@gmail.com>
2023-11-25 14:12:35 +0100

    Start of project

---
+++ encrypter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+function encrypt(text, shift):
+    for each character in text:
+        if character is a letter:
+            shift the character by 'shift' positions
+        end if
+    end for
+    return the encrypted text
+end function
+
+*/
+
