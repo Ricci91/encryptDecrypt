347ac71e13dc8f35f29b60e2bee3ba2d83cb332f
Ricci91 <froyland.richard@gmail.com>
2023-11-25 17:37:40 +0100

    Laget encrypt funksjon som jeg tror funker og stringlength funksjon som jeg vet funker

--- encrypter.c
+++ encrypter.c
@@ -1,15 +1,64 @@
 #include <stdio.h>
 #include <ctype.h>
 
-/**
-function encrypt(text, shift):
-    for each character in text:
-        if character is a letter:
-            shift the character by 'shift' positions
-        end if
-    end for
-    return the encrypted text
-end function
 
+
+//Function prototypes
+void encrypt(char text[],int shift);
+int stringlength(char text[]);
+
+
+//Main Program
+int main() {
+    char text[] = "Hello, world";
+    int shift = 1;
+
+    printf("Original text: %s.\n", text);
+    encrypt(text, shift);
+    printf("Encrypted text: %s.\n", text);
+
+    return 0;
+}
+
+/**
+ * @brief Encrypts input text with a simple caesar cipher
+ * 
+ * This function takes input text and a shift number to encrypt a string 
+ * with a simple caesar cipher.
+ * 
+ * @param text The text to encrypt
+ * @param shift The number of positions to shift each character
+ * 
 */
 
+void encrypt(char text[], int shift) {
+    int length = stringlength(text);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (isalpha(text[i])) 
+        {
+            char base = 'A';
+            if (islower(text[i]))
+            {
+                char base = 'a';
+            }
+            text[i] = (text[i] - base + shift) %26 + base;
+        } 
+    }
+}
+
+/**
+ * @brief Gets the length of a string
+ * 
+ * @param text The text string that should be measured
+*/
+
+int stringlength(char text[]) {
+    int length = 0;
+    while (text[length] != '\0') 
+    {
+        length++;
+    }
+    return length;
+}
